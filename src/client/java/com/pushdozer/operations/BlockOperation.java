c9978f847808eb7a07ee4eb08f56ec3a0937e2d8
Theopote <hillpote@gmail.com>
2024-12-23 12:52:05 +0800

    12.23

--- src/client/java/com/pushdozer/operations/BlockOperation.java
+++
@@ -1,8 +0,0 @@
-package com.pushdozer.operations;
-
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.world.World;
-
-public interface BlockOperation {
-    void execute(PlayerEntity player, World world);
-}
